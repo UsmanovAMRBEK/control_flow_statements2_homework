150cf9e8e379edecececec4f23375871e1efda31
unknown <princariens@gmail.com>
2022-03-24 10:20:48 +0500

    06

--- if06.py
+++ if06.py
@@ -1,31 +1,26 @@
+from operator import index
+
+
 def main(n):
     """
     Find the index of the largest digit of the five-digit number.
     Args:
         n: Five-digit number.
     Returns:
-        int: return answer.
+        int: return answer.  # 57643
     """
-    max=n%10
-    n=n//10
+    max=n//10000
+    index=5
+    if n//1000%10>max:
+        max=n//1000%10
+        index=4
+    if n//100%10>max:
+        max=n//100%10
+        index=3
+    if n//10%10>max:
+        max=n//10%10
+        index=2
     if n%10>max:
         max=n%10
         index=1
-    n=n//10
-    if n%10>max:
-        max=n%10
-        index=2
-    n=n//10
-    if n%10>max:
-        max=n%10
-        index=3
-    n=n//10
-    if n%10>max:
-        max=n%10
-        index=4
-    n=n//10
-    if n%10>max:
-        max=n%10
-        index=5
-    
     return index

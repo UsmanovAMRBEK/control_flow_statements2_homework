ae94f485189d91952011a39d6fbcbe0bc5c18ea6
unknown <princariens@gmail.com>
2022-03-24 09:59:57 +0500

    07

--- if06.py
+++ if06.py
@@ -6,4 +6,26 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    max=n%10
+    n=n//10
+    if n%10>max:
+        max=n%10
+        index=1
+    n=n//10
+    if n%10>max:
+        max=n%10
+        index=2
+    n=n//10
+    if n%10>max:
+        max=n%10
+        index=3
+    n=n//10
+    if n%10>max:
+        max=n%10
+        index=4
+    n=n//10
+    if n%10>max:
+        max=n%10
+        index=5
+    
+    return index

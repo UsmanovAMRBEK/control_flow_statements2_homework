2476e64ef7f57a4df2dccba8177cce5ba52b4bf8
unknown <princariens@gmail.com>
2022-03-24 10:13:04 +0500

    change 02

--- if02.py
+++ if02.py
@@ -8,9 +8,13 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    min=a
-    if b<=min:
-        min=b
-    elif c<=min:
-        min=c
-    return min
+    if a<b:
+        if a<c:
+            return a
+        else:
+            return c
+    else:
+        if b<c:
+            return b
+        else:
+            return c
